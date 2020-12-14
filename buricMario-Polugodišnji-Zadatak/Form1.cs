a924f1a6664ebf1e56dd075f74f872b26d212891
MarioBuric <55134593+MarioBuric@users.noreply.github.com>
2020-12-14 23:57:57 +0100

    Add files via upload

---
+++ buricMario-Polugodišnji-Zadatak/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace buricMario_Polugodišnji_Zadatak
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string marka = textBox1.Text;
+            string model = textBox2.Text;
+            string Godiste = textBox3.Text;
+
+            if (string.IsNullOrWhiteSpace(marka) || string.IsNullOrWhiteSpace(model) || string.IsNullOrWhiteSpace(Godiste)) ;
+            {
+                MessageBox.Show("Pogrešni podaci");
+                return;
+                int godiste = Convert.ToInt32(Godiste);
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < Vozila.Count; i++)
+                if (Vozila[i].Godiste == 2020)
+                {
+                    Vozila[i].Status = "Novo";
+                }
+                else if (Vozila[i].Godiste > 1990)
+                {
+                    Vozila[i].Status = "Oldtimer";
+                }
+                else
+                {
+                    (Vozila[i].Godiste > 1990);
+
+                    Vozila[i].Status = "Skoro Novo";
+                }
+
+                
+
+
+
+
+
+
+        }
+
+            private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox1.Items.Add("Marka          Model          Godiste ");
+             
+            {
+                listBox1.Items.Add(Vozila.ToString());
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+        
+    
+    
+
+
---
+++ buricMario-Polugodišnji-Zadatak/Vozila.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buricMario_Polugodišnji_Zadatak
+{
+    class Vozila
+    {
+        string marka;
+        string model;
+        int godiste;
+        
+
+        public string Marka { get => marka; set => marka = value; }
+        public string Model { get => model; set => model = value; }
+        public int Godiste { get => godiste; set => godiste = value; }
+        public static int Count { get; internal set; }
+
+        public Vozila(string marka, string model, int godiste)
+        {
+            this.marka = marka;
+            this.model = model;
+            this.godiste = godiste;
+            
+        }
+    }
+}
